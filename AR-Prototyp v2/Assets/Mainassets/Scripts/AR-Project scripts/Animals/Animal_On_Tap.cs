9ca59cc465c271cedbe7518f45e28464eca0d44b
Keros918 <haasleon.s@gmx.de>
2023-10-12 20:34:37 +0200

    Add on tap script, lion animation and lion info text

---
+++ AR-Prototyp v2/Assets/Mainassets/Scripts/AR-Project scripts/Animals/Animal_On_Tap.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Animal_On_Tap : MonoBehaviour
+{
+    private Animator animator;
+    public Camera mainCamera;
+    public TMP_Text infoText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        infoText.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount == 0 || Input.GetTouch(0).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        Ray ray = mainCamera.ScreenPointToRay(Input.GetTouch(0).position);
+        if (Physics.Raycast(ray, out _))
+        {
+            animator.SetTrigger("OnTap");
+            infoText.gameObject.SetActive(true);
+        }
+    }
+}
